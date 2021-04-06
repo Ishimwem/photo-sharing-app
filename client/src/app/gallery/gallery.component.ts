b10ab10b89da63e08ace7f3ee72107ecfd065c56
Ishimwem <ishimwe.merci@gmail.com>
2021-04-06 00:44:12 -0400

    polishing the UI

--- client/src/app/gallery/gallery.component.ts
+++ client/src/app/gallery/gallery.component.ts
@@ -3,8 +3,8 @@ import { Photo } from '../photo';
 import { GalleryService } from './gallery.service';
 import { NzUploadChangeParam } from 'ng-zorro-antd/upload';
 import { NzMessageService } from 'ng-zorro-antd/message';
-import { EMPTY, Observable, of } from 'rxjs'
-import { catchError, map, tap } from 'rxjs/operators'
+import { EMPTY} from 'rxjs'
+import { catchError, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-gallery',
@@ -12,14 +12,12 @@ import { catchError, map, tap } from 'rxjs/operators'
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
-
   photos: Photo[];
   inProgress: boolean = false;
 
-
   constructor(
     private galleryService: GalleryService,
-    private messageService: NzMessageService
+    private messageService: NzMessageService,
   ) { }
 
   ngOnInit(): void {
@@ -27,21 +25,18 @@ export class GalleryComponent implements OnInit {
   }
 
   getPhotos() {
-    console.log("getting in getPhotos in components");
     this.galleryService.getPhotos().pipe(
       tap((photos: Photo[]) => {
         this.photos = photos;
-        console.log(this.photos);
       }),
       catchError((response: any) => {
-        console.log('got an error'); // TODO: put better error handling
+        this.messageService.error('an error occured when retrieving photos');
         return EMPTY;
       })
     ).subscribe();
   }
 
   deletePhoto(photoId: string) {
-    console.log('got into handleDelete');
     this.galleryService.deletePhoto(photoId).pipe(
       tap(() => {
         this.messageService.success('photo deleted successfully');
@@ -49,33 +44,23 @@ export class GalleryComponent implements OnInit {
       }),
       catchError((response: any) => {
         this.messageService.error('an error occured when deleting the photo');
-        console.log('got an error'); // TODO: put better error handling: use error message??
         return EMPTY;
       })
     ).subscribe();
 
   }
 
-  handlePreview() {
-    console.log('got into handlePreview');
-
-  }
-
   uploadPhoto(photo: any): any {
-    console.log('got into handleUpload');
-    // console.log({photo});
-    console.log(photo.file);
     const formData = new FormData();
     formData.append('photo', photo.file);
 
     this.galleryService.uploadPhoto(formData).pipe(
       tap((response: any) => {
-        console.log(response);
         //this.getPhotos();
         photo.onSuccess(response.file);
       }),
       catchError((response: any) => {
-        console.log('got an error'); // TODO: put better error handling
+        this.messageService.error('an error occured when uploading the photo');
         return EMPTY;
       })
     ).subscribe();
@@ -95,6 +80,4 @@ export class GalleryComponent implements OnInit {
       this.messageService.error(`${info.file.name} file upload failed.`);
     }
   }
-
-
 }
--- client/src/app/gallery/gallery.service.ts
+++ client/src/app/gallery/gallery.service.ts
@@ -17,68 +17,6 @@ export class GalleryService {
 
   getPhotos() {
     return this.http.get<Photo[]>(this.photoApi);
-    const photos = [{
-      uid: '1',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1051&q=80',
-    },
-    {
-      uid: '2',
-      name: 'image.png',
-      status: 'done',
-      url:'https://images.unsplash.com/photo-1470071459604-3b5ec3a7fe05?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1140&q=80',
-    },
-    {
-      uid: '3',
-      name: 'image.png',
-      status: 'done',
-      url: 'https://images.unsplash.com/photo-1588392382834-a891154bca4d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1055&q=80'
-    }, 
-    {
-      uid: '4',
-      name: 'image.png',
-      status: 'done', 
-      url:'https://images.unsplash.com/photo-1586348943529-beaae6c28db9?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=658&q=80'
-    },
-    {
-      uid: '5',
-      name: 'image.png',
-      status: 'done', 
-      url:'https://images.unsplash.com/photo-1433086966358-54859d0ed716?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8N3x8bmF0dXJlfGVufDB8fDB8&auto=format&fit=crop&w=600&q=60'
-    }, 
-    {
-      uid: '6', 
-      name: 'image.png',
-      status: 'done',
-      url:'https://images.unsplash.com/photo-1540206395-68808572332f?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8Nnx8bmF0dXJlfGVufDB8fDB8&auto=format&fit=crop&w=600&q=60'
-    }, 
-    {
-      uid: '7',
-      name: 'image.png',
-      status: 'done', 
-      url: 'https://images.unsplash.com/photo-1418065460487-3e41a6c84dc5?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8OXx8bmF0dXJlfGVufDB8fDB8&auto=format&fit=crop&w=600&q=60'
-    }, 
-    {
-      uid: '8',
-      name: 'image.png',
-      status: 'done', 
-      url:'https://images.unsplash.com/photo-1505765050516-f72dcac9c60e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTN8fG5hdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
-    }, 
-    {
-      uid: '9',
-      name: 'image.png',
-      status: 'done', 
-      url:'https://images.unsplash.com/photo-1500534623283-312aade485b7?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTV8fG5hdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
-    }, 
-    {
-      uid: '10', 
-      name: 'image.png',
-      status: 'done',
-      url:'https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTh8fG5hdHVyZXxlbnwwfHwwfA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=600&q=60'
-    }];
-
-    // return photos;
   }
 
   deletePhoto(photoId: string) {
@@ -86,7 +24,6 @@ export class GalleryService {
   }
 
   uploadPhoto(photo: FormData) {
-    console.log("getting into upload photo")
     return this.http.post(`${this.photoApi}`, photo);
   }
 }
